3ac2a5490e8fe7880715abb69f3cd678290b298b
Alexander Black <alexwhb@gmail.com>
2020-05-12 10:13:06 -0400

    Updates setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="PySchemaGen",
+    name="pyschemagen",
     version="0.0.1",
     author='alexwhb',
     description="A package to generate orator DB schemas from a python dict.",
